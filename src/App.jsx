b13bd431bb40d74ae747fbb82a10aa18acbd5746
indupshivamyt <ind.up.shivamyt@gmail.com>
2026-02-20 12:28:21 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Auth from "./pages/Auth";
-import Dashboard from "./pages/Dashboard";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1 style={{color: "red"}}>NEW VERSION WORKING</h1>;
 }
 
 export default App;
